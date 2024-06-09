115e5769b605dde476de2f36e5fd377d40af8d60
Jeongbowoon <happyanne200212@gmail/com>
2024-06-10 02:51:07 +0900

    sytle: toggle button 추가

--- note-project/src/components/Title/Title.tsx
+++ note-project/src/components/Title/Title.tsx
@@ -9,26 +9,27 @@ const TItle = () => {
     bodyRef.current = document.body as HTMLBodyElement;
   });
 
-  function handleBtnClick() {
-
+  const handleBtnClick = () => {
     const newTheme = theme === "light-mode" ? "dark-mode" : "light-mode";
     setTheme(newTheme);
 
     if (bodyRef.current) {
       bodyRef.current.setAttribute("data-theme", newTheme);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>HB's Notes App</h1>
-      <span className={styles.span}>나만의 노트를 생성해보세요!</span>
-      <div
-        onClick={handleBtnClick}
-        className={theme === 'light-mode'? styles.toggleBtn : styles.toggleBtnClick}
-      >
-        토글버튼입니다
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>HB's Notes</h1>
+        <span className={styles.span}>나만의 노트를 생성해보세요!</span>
       </div>
+
+      <img
+        onClick={handleBtnClick}
+        className={styles.toggleBtn}
+        src={theme === "light-mode" ? "toggle_on.svg" : "toggle_off.svg"}
+      ></img>
     </div>
   );
 };
