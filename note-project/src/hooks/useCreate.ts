0b22f3d472b5e6d0f679248c61e857ef53e4f491
imhwarang <imhwarang4@naver.com>
2024-06-10 01:39:36 +0900

    fix: Done 버튼 클릭 후 입력칸 비우기

---
+++ note-project/src/hooks/useCreate.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { saveNote } from "../utils/storageNote";
+
+const useCreate = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const handleContentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+  const handleDoneClick = () => {
+    if (title.trim().length === 0 || content.trim().length === 0) {
+      alert("빈칸을 정확히 입력해주세요.");
+      return;
+    }
+    if (title && content) {
+      const newNote = { title, content };
+      saveNote(newNote);
+      setTitle("");
+      setContent("");
+    }
+  };
+  return {
+    title,
+    content,
+    setTitle,
+    setContent,
+    handleTitleChange,
+    handleContentChange,
+    handleDoneClick,
+  };
+};
+export default useCreate;
--- note-project/src/pages/Create/Create.tsx
+++ note-project/src/pages/Create/Create.tsx
@@ -2,24 +2,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Create.module.css";
 import Button from "../../components/commons/Button/Button";
-import useChange from "../../hooks/useChange";
-import { saveNote } from "../../utils/storageNote";
+import useCreate from "../../hooks/useCreate";
 
 const Create = () => {
-  const { title, content, handleTitleChange, handleContentChange } =
-    useChange();
+  const {
+    title,
+    content,
+    handleTitleChange,
+    handleContentChange,
+    handleDoneClick,
+  } = useCreate();
   const handleRemoveNote = () => {
     console.log("remove note");
-  };
-  const handleDoneClick = () => {
-    if (title.trim().length === 0 || content.trim().length === 0) {
-      alert("빈칸을 정확히 입력해주세요.");
-      return;
-    }
-    if (title && content) {
-      const newNote = { title, content };
-      saveNote(newNote);
-    }
   };
   return (
     <section className={styles.container}>
@@ -31,11 +25,13 @@ const Create = () => {
       <main className={styles.main}>
         <input
           type="text"
+          value={title}
           className={styles.title}
           placeholder="제목을 입력하세요."
           onChange={handleTitleChange}
         />
         <textarea
+          value={content}
           className={styles.content}
           placeholder="내용을 입력하세요."
           onChange={handleContentChange}
