0e7876507c15ddf83284814a2464efa95a4b5032
imhwarang <imhwarang4@naver.com>
2024-06-09 22:38:54 +0900

    style: Title 컴포넌트 스타일링

--- note-project/src/pages/Create/Create.tsx
+++ note-project/src/pages/Create/Create.tsx
@@ -1,5 +1,7 @@
+import styles from './Create.module.css'
+
 const Create = () => {
-  return <div>Create</div>;
+  return <div className={styles.container}>Create</div>;
 };
 
 export default Create;
