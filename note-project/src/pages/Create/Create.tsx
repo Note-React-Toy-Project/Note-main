fa46aba5865c877401e9aefc97c84fed15ccc765
imhwarang <imhwarang4@naver.com>
2024-06-10 00:37:51 +0900

    feat: create페이지 입력칸 localStorage 저장

--- note-project/src/pages/Create/Create.tsx
+++ note-project/src/pages/Create/Create.tsx
@@ -2,13 +2,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Create.module.css";
 import Button from "../../components/commons/Button/Button";
+import useChange from "../../hooks/useChange";
+import { saveNote } from "../../utils/storageNote";
 
 const Create = () => {
+  const { title, content, handleTitleChange, handleContentChange } =
+    useChange();
   const handleRemoveNote = () => {
     console.log("remove note");
   };
   const handleDoneClick = () => {
-    console.log("done click");
+    if (title.trim().length === 0 || content.trim().length === 0) {
+      alert("빈칸을 정확히 입력해주세요.");
+      return;
+    }
+    if (title && content) {
+      const newNote = { title, content };
+      saveNote(newNote);
+    }
   };
   return (
     <section className={styles.container}>
@@ -18,8 +29,17 @@ const Create = () => {
         </button>
       </nav>
       <main className={styles.main}>
-        <input type="text" className={styles.title} placeholder="제목을 입력하세요." />
-        <textarea className={styles.content} placeholder="내용을 입력하세요." />
+        <input
+          type="text"
+          className={styles.title}
+          placeholder="제목을 입력하세요."
+          onChange={handleTitleChange}
+        />
+        <textarea
+          className={styles.content}
+          placeholder="내용을 입력하세요."
+          onChange={handleContentChange}
+        />
       </main>
       <footer className={styles.footer}>
         <Button
